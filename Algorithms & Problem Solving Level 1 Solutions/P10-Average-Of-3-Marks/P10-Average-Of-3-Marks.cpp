a5334da5e73a7577e615e89c3abe4d3431f7f2a7
anaschetoui <embratoriahd@gmail.com>
2025-03-07 10:42:12 +0000

    create P10

---
+++ Algorithms & Problem Solving Level 1 Solutions/P10-Average-Of-3-Marks/P10-Average-Of-3-Marks.cpp
@@ -0,0 +1,47 @@
+// P10-Average-Of-3-Marks
+
+#include <iostream>
+using namespace std;
+
+float ReadFloatNumber(string Message)
+{
+	float Number=0.000f;
+
+	do
+	{
+		cout << Message;
+		cin >> Number;
+	} while (Number < 0);
+	
+	return Number;
+}
+
+void ReadMarks(float& Mark1, float& Mark2, float& Mark3)
+{
+	Mark1 = ReadFloatNumber("Enter Mark1: ");
+	Mark2 = ReadFloatNumber("Enter Mark2: ");
+	Mark3 = ReadFloatNumber("Enter Mark3: ");
+}
+
+float CalculateMarksAverage(float Mark1, float Mark2, float Mark3)
+{
+	return (Mark1 + Mark2 + Mark3) / 3;
+}
+
+void PrintMarksAverage(float Average)
+{
+	cout << "avg = " << Average;
+}
+
+int main()
+{
+	float Mark1, Mark2, Mark3;
+
+	ReadMarks(Mark1, Mark2, Mark3);
+
+	float Avg = CalculateMarksAverage(Mark1, Mark2, Mark3);
+
+	PrintMarksAverage(Avg);
+
+	return 0; 
+}
