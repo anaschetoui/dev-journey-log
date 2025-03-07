fa0d1afa1c1f0273e7ac2bd94a338328318b169f
anaschetoui <embratoriahd@gmail.com>
2025-03-07 10:40:19 +0000

    create P01

---
+++ Algorithms & Problem Solving Level 1 Solutions/P01-Print-Your-Name/P01-Print-Your-Name.cpp
@@ -0,0 +1,17 @@
+// P01-Print-Your-Name
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void PrintYourName(string Name)
+{
+	cout << "Your Name is " << Name;
+}
+
+int main()
+{
+	PrintYourName("Anas Chetoui\n");
+
+	return 0;
+}
