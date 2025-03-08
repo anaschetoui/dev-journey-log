e12912f0a9c9503bfc744b2f5c11d55e848ec602
anaschetoui <embratoriahd@gmail.com>
2025-03-08 00:15:08 +0000

    create P19

---
+++ Algorithms & Problem Solving Level 1 Solutions/P19-Circle-Area-By-Diameter/P19-Circle-Area-By-Diameter.cpp
@@ -0,0 +1,41 @@
+// P19-Circle-Area-By-Diameter
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float ReadPositiveNumber(string Message)
+{
+	float Num = 0;
+
+	do
+	{
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+
+	return Num;
+}
+
+float CircleAreaByDiameter(float D)
+{
+	const double PI = 3.14159265358979323846;
+	float Area = (PI * (pow(D, 2)))/4;
+
+	return Area;
+}
+
+void PrintArea(float Area)
+{
+	cout << "Circle Area = " << Area;
+}
+
+int main()
+{
+	float D = ReadPositiveNumber("Enter D: ");
+
+	PrintArea(CircleAreaByDiameter(D));
+
+	return 0;
+}
+
