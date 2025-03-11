2b11bf526af83b43731df37cfcb6563e8e2370a2
anaschetoui <embratoriahd@gmail.com>
2025-03-11 19:17:03 +0000

    Create P46

---
+++ Algorithms & Problem Solving Level 1 Solutions/P46-Print-From-A-Z/P46-Print-From-A-Z.cpp
@@ -0,0 +1,21 @@
+// P46-Print-From-A-Z
+
+#include <iostream>
+using namespace std;
+
+void PrintAllLetters()
+{
+	for (char i = 'A'; i <= 'Z'; i++)
+	{
+		cout << i << endl;
+	}
+}
+
+int main()
+{
+	PrintAllLetters();
+
+	return 0;
+}
+
+
