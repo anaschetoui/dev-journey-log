d492cb6eaeca686a07d97e1bc147c5ea173371cb
anaschetoui <embratoriahd@gmail.com>
2025-03-14 11:23:17 +0000

    create Problem 14

---
+++ Algorithms & Problem Solving Level 2/P14-Inverted-Letter-Pattern/P14-Inverted-Letter-Pattern.cpp
@@ -0,0 +1,39 @@
+// P14-Inverted-Letter-Pattern
+
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+	int Number = 0;
+	do {
+		cout << Message;
+		cin >> Number;
+	} while (Number < 1 || Number >26);
+
+	return Number;
+}
+
+
+void PrintLetterPattern(int Number)
+{
+
+	for (int i = Number+64; i >= 65; i--)
+	{
+		for (int j = 65; j <= i; j++)
+		{
+			cout <<char(i);
+		}
+
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int Number = ReadPositiveNumber("Enter a Positive Number (1-26): ");
+
+	PrintLetterPattern(Number);
+
+	return 0;
+}
