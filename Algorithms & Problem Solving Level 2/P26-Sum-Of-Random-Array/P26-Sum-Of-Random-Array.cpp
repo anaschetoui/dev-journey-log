22a7c55b8fd5a22044372a65b1a497de96ff986f
anaschetoui <embratoriahd@gmail.com>
2025-03-15 11:52:19 +0000

    create Problem 26

---
+++ Algorithms & Problem Solving Level 2/P26-Sum-Of-Random-Array/P26-Sum-Of-Random-Array.cpp
@@ -0,0 +1,61 @@
+// P26-Sum-Of-Random-Array
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int RandomNumber(int from, int to)
+{
+	return rand() % (to - from + 1) + from;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+	cout << "Enter How many Elements do you want: ";
+	cin >> arrLength;
+
+	for (int i = 0; i < arrLength; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+
+void PrintArray(int arr[100], int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << '\n';
+}
+
+
+int SumOfRandomArray(int arr[100], int arrLength)
+{
+	int Sum = 0;
+	for (int i = 0; i < arrLength; i++)
+	{
+		Sum += arr[i];
+	}
+
+	return Sum;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100], arrLength;
+
+	FillArrayWithRandomNumbers(arr, arrLength);
+
+	cout << "\nArray Elements: ";
+	PrintArray(arr, arrLength);
+
+	cout << "Sum = " << SumOfRandomArray(arr, arrLength);
+
+
+	return 0;
+}
