4e8bb31c99da84949fab09ac8d4f7b8cfa4910c4
anaschetoui <embratoriahd@gmail.com>
2025-07-28 21:44:13 +0100

    Add program to calculate number of days (two line logic code) in a given month and year with input validation

--- Algorithms & Problem Solving Level 4/P06_NumberOfDaysInMonth(Two_Line_code)/P06_NumberOfDaysInMonth(Two_Line_code).cpp
+++ Algorithms & Problem Solving Level 4/P06_NumberOfDaysInMonth(Two_Line_code)/P06_NumberOfDaysInMonth(Two_Line_code).cpp
@@ -45,12 +45,12 @@ short ReadMonth(std::string Message = "Enter a Month (1-12): ")
 
 bool IsLeapYear(short Year)
 {
-    return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+	return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
 }
 
 short NumberOfDaysInMonth(short Month, short Year)
 {
-    int DaysInMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int DaysInMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : DaysInMonth[Month - 1];
 }
 
